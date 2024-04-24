6006200a994533f75a9e8be09d3fde491db5692d
Addison Douglas <87285846+AddisonDouglas@users.noreply.github.com>
2024-04-23 18:20:27 -0700

    Add files via upload

---
+++ Assets/Scripts/FieldOfView.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FieldOfView : MonoBehaviour
+{
+    public float viewRadius;
+
+    [Range(0, 360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    public List<Transform> visibleTargets = new List<Transform>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("FindTargetsWithDelay", .2);
+    }
+
+    IEnumerator FindTargetsWithDelay(float delay)
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(delay);
+            FindVisibleTargets();
+        }
+    }
+    void FindVisibleTargets()
+    {
+        visibleTargets.Clear();
+        Collider2D[] targetsInViewRadius = Physics2D.OverlapCircleAll(transform.position,viewRadius, targetMask);
+
+        for(int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform Target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (Target.position - transform.position).normalized;
+            if(Vector3.Angle (transform.up, dirToTarget) < viewAngle/2)
+            {
+                float disToTarget = Vector3.Distance (transform.position, Target.position);
+                if (!Physics.Raycast (transform.position, dirToTarget, disToTarget, obstacleMask))
+                {
+                    visibleTargets.Add(Target);
+                }
+            }
+        }
+
+    }
+
+    public Vector2 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees -= transform.eulerAngles.z;
+        }
+        return new Vector2(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+
+    }
+}
