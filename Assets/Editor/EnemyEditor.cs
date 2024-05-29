c1c12d51d5b1be11075031a1c0238f27d11bc663
Kyler Li <loukylor@gmail.com>
2024-05-28 23:07:48 -0700

    fix enemy pathes not saving/undoing right

--- Assets/Editor/EnemyEditor.cs
+++ Assets/Editor/EnemyEditor.cs
@@ -11,11 +11,14 @@ namespace OfficeFood.Enemy
             Enemy enemy = (Enemy)target;
 
             // Draw patrol point things
+            Undo.RecordObject(enemy, "change patrol point");
             for (int i = 0; i < enemy.patrolPoints.Length; i++)
             {
                 enemy.patrolPoints[i] = Handles.FreeMoveHandle(enemy.patrolPoints[i], 0.1f, Vector3.zero, Handles.DotHandleCap);
                 Handles.DrawLine(enemy.patrolPoints[i], enemy.patrolPoints[(i + 1) % enemy.patrolPoints.Length]);
             }
+
+            PrefabUtility.RecordPrefabInstancePropertyModifications(enemy);
         }
 
         public override void OnInspectorGUI()
